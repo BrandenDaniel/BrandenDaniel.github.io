b4b24864feece87ceb17404f359021637e595d66
Branden Ng <brandendanielng@gmail.com>
2020-02-28 18:23:11 +1100

    homepage landing

--- wp-content/themes/child/template-parts/homepage-content.php
+++ wp-content/themes/child/template-parts/homepage-content.php
@@ -1,7 +1,7 @@
 
 <div class="video-container">
     <video loop="loop" muted="muted" autoplay="autoplay" playsinline="" class="landing-background-video"><source type="video/mp4" src="wp-content\themes\child\src\video\placeholder-video.mp4"></video>
-    <h1><span class="brand">uncle funk</span><br>a new array of sauces<br><span class="sauce">coming to Melbroune.</span></h1>
+    <h1><span class="brand">uncle funk</span><br>a new array of sauces coming to Melbroune.</h1>
 </div>
 
 <div class="container container--homepage-content">
